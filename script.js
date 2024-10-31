4b2672476544dce69af5f23ee801cf5327364f76
Fatir Yousaf <fatiryousaf@gmail.com>
2024-10-31 18:19:01 -0400

    Gave search button and value unique classes and added event listener for search button to fetch weather detail for city name (API not activated yet)

---
+++ script.js
@@ -0,0 +1,13 @@
+var searchButton = document.querySelector('.searchButton');
+var searchInput = document.querySelector('.searchValue');
+
+searchButton.addEventListener('click', function() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchInput +'&appid=e9fdbec485cb1bb68cbd3557feb65d88')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        alert("Please enter a valid city name");
+    });
+});
