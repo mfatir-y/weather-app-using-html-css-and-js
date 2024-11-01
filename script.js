a9c85ce1e3b6c1cd499750fa0ad9e9c4baecd27b
Fatir Yousaf <fatiryousaf@gmail.com>
2024-11-01 14:27:57 -0400

    Fixed small bug and successfully called API in console using user input

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var searchButton = document.querySelector('.searchButton');
 var searchInput = document.querySelector('.searchValue');
 
 searchButton.addEventListener('click', function() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchInput +'&appid=e9fdbec485cb1bb68cbd3557feb65d88')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchInput.value+'&appid=e9fdbec485cb1bb68cbd3557feb65d88')
     .then(response => response.json())
     .then(data => {
         console.log(data);
