a608818a6a5a129687b625afe3b9ed7166c39622
Fatir Yousaf <fatiryousaf@gmail.com>
2024-12-15 14:32:38 -0500

    Added support for enter click aswell. The search input clears itself after each api call

--- script.js
+++ script.js
@@ -4,17 +4,28 @@ var cityName = document.querySelector('.city-name');
 var temperature = document.querySelector('.temperature');
 var description = document.querySelector('.description');
 
-searchButton.addEventListener('click', function() {
+function fetchWeatherData() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchInput.value + '&appid=e9fdbec485cb1bb68cbd3557feb65d88&units=metric')
-    .then(response => response.json())
-    .then(data => {
-        var nameValue = data['name'];
-        var tempValue = data['main']['temp'];
-        var descValue = data['weather'][0]['description'];
+        .then(response => response.json())
+        .then(data => {
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var descValue = data['weather'][0]['description'];
 
-        cityName.innerHTML = nameValue;
-        temperature.innerHTML = tempValue + ' °C';
-        description.innerHTML = descValue;
-    })
-    .catch(e => alert("Please enter a valid city name."));
+            cityName.innerHTML = nameValue;
+            temperature.innerHTML = tempValue + ' °C';
+            description.innerHTML = descValue;
+        })
+        .catch(e => alert("Please enter a valid city name."))
+        .finally(() => {
+            searchInput.value = '';
+        });
+}
+
+searchButton.addEventListener('click', fetchWeatherData);
+
+searchInput.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        fetchWeatherData();
+    }
 });
