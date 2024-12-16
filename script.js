905c02f7129b1d131b2654dba9b5a607926730c9
Fatir Yousaf <fatiryousaf@gmail.com>
2024-12-16 15:58:55 -0500

    Added windspeed and number of daylight hours in the given location

--- script.js
+++ script.js
@@ -17,52 +17,62 @@ function createCityCard(data) {
     var card = document.createElement('div');
     card.className = 'card text-white bg-dark h-100 text-center shadow rounded-3';
 
-    var cityName = document.createElement('h4');
-    cityName.className = 'city-name m-0';
-    cityName.textContent = data['name'];
-    var countryName = document.createElement('p');
-    countryName.className = 'country-name m-0 fs-6 fw-light';
-    countryName.textContent = data['sys']['country'];
+    var cardHeader = document.createElement('div');
+    cardHeader.className = 'card-header pb-0 d-flex flex-direction-row justify-content-around align-items-center';
 
     var nameGroup = document.createElement('div');
     nameGroup.className = 'text-start';
+    var cityName = document.createElement('h4');
+    cityName.className = 'city-name m-0';
+    cityName.textContent = data['name'];
     nameGroup.appendChild(cityName);
+    var countryName = document.createElement('p');
+    countryName.className = 'country-name m-0 fs-6 fw-light';
+    countryName.textContent = data['sys']['country'];
     nameGroup.appendChild(countryName);
-
-    var icon = document.createElement('img');
-    icon.className = 'weather-icon';
-    icon.src = 'https://openweathermap.org/img/wn/' + data['weather'][0]['icon'] + '.png';
-
-    var cardHeader = document.createElement('div');
-    cardHeader.className = 'card-header d-flex flex-direction-row justify-content-around align-items-center';
     cardHeader.appendChild(nameGroup);
-    cardHeader.appendChild(icon);
+    var weatherIcon = document.createElement('img');
+    weatherIcon.className = 'weather-icon';
+    weatherIcon.src = 'https://openweathermap.org/img/wn/' + data['weather'][0]['icon'] + '.png';
+    cardHeader.appendChild(weatherIcon);
 
     var cardBody = document.createElement('div');
     cardBody.className = 'card-body';
 
-    var temperatureElement = document.createElement('p');
-    temperatureElement.className = 'temperature card-text fw-bold';
-    temperatureElement.textContent = data['main']['temp'] + ' °C';
-    var feelsLikeElement = document.createElement('p');
-    feelsLikeElement.className = 'feels-like card-text fst-italic';
-    feelsLikeElement.textContent = 'Feels Like: ' + data['main']['feels_like'] + ' °C';
-
     var tempElements = document.createElement('div');
     tempElements.className = 'd-flex justify-content-around';
+    var temperatureElement = document.createElement('p');
+    temperatureElement.className = 'temperature card-text fw-bold';
+    temperatureElement.textContent = `${data['main']['temp']} °C`;
     tempElements.appendChild(temperatureElement);
+    var feelsLikeElement = document.createElement('p');
+    feelsLikeElement.className = 'feels-like card-text fst-italic';
+    feelsLikeElement.textContent = `Feels Like: ${data['main']['feels_like']} °C`;
     tempElements.appendChild(feelsLikeElement);
+    cardBody.appendChild(tempElements);
+    
+    var daylightDuration = data.sys.sunset - data.sys.sunrise;
+    var daylightHours = Math.floor(daylightDuration / 3600);
+    var daylightMinutes = Math.floor((daylightDuration % 3600) / 60);
+    
+    var daylightElement = document.createElement('p');
+    daylightElement.className = 'daylight card-text';
+    daylightElement.textContent = `Daylight Duration: ${daylightHours} hrs ${daylightMinutes} mins`;
+    cardBody.appendChild(daylightElement);
+
+    var windElement = document.createElement('p');
+    windElement.className = 'wind card-text';
+    windElement.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+    cardBody.appendChild(windElement);
 
     var descElement = document.createElement('p');
-    descElement.className = 'description card-text text-capitalize';
+    descElement.className = 'description card-text text-capitalize fw-bold';
     descElement.textContent = data['weather'][0]['description'];
-
-    cardBody.appendChild(tempElements);
     cardBody.appendChild(descElement);
+
     card.appendChild(cardHeader);
     card.appendChild(cardBody);
     col.appendChild(card);
-
     citiesSearchedContainer.appendChild(col);
 }
 
