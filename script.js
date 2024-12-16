c31d8e1a4a74b84f6b8159fe911479e123a110ba
Fatir Yousaf <fatiryousaf@gmail.com>
2024-12-16 14:40:21 -0500

    Added weather icons, feels like temp., and country codes. Changed card theme to dark.

--- script.js
+++ script.js
@@ -3,42 +3,67 @@ var searchInput = document.querySelector('.searchValue');
 var cityName = document.querySelector('.city-name');
 var temperature = document.querySelector('.temperature');
 var description = document.querySelector('.description');
-var resultsContainer = document.querySelector('.row');
+var citiesSearchedContainer = document.querySelector('.row');
 
 function cityExists(city) {
     var cityHeaders = document.querySelectorAll('.city-name');
     return Array.from(cityHeaders).some(header => header.textContent.toLowerCase() === city.toLowerCase());
 }
 
-function createCityCard(name, temp, desc) {
+function createCityCard(data) {
     var col = document.createElement('div');
     col.className = 'col';
     
     var card = document.createElement('div');
-    card.className = 'card h-100 text-center shadow rounded-3';
+    card.className = 'card text-white bg-dark h-100 text-center shadow rounded-3';
 
-    var cardHeader = document.createElement('h4');
-    cardHeader.className = 'card-header city-name';
-    cardHeader.textContent = name;
+    var cityName = document.createElement('h4');
+    cityName.className = 'city-name m-0';
+    cityName.textContent = data['name'];
+    var countryName = document.createElement('p');
+    countryName.className = 'country-name m-0 fs-6 fw-light';
+    countryName.textContent = data['sys']['country'];
+
+    var nameGroup = document.createElement('div');
+    nameGroup.className = 'text-start';
+    nameGroup.appendChild(cityName);
+    nameGroup.appendChild(countryName);
+
+    var icon = document.createElement('img');
+    icon.className = 'weather-icon';
+    icon.src = 'https://openweathermap.org/img/wn/' + data['weather'][0]['icon'] + '.png';
+
+    var cardHeader = document.createElement('div');
+    cardHeader.className = 'card-header d-flex flex-direction-row justify-content-around align-items-center';
+    cardHeader.appendChild(nameGroup);
+    cardHeader.appendChild(icon);
 
     var cardBody = document.createElement('div');
     cardBody.className = 'card-body';
 
-    var tempElement = document.createElement('p');
-    tempElement.className = 'temperature card-text';
-    tempElement.textContent = temp + ' °C';
+    var temperatureElement = document.createElement('p');
+    temperatureElement.className = 'temperature card-text fw-bold';
+    temperatureElement.textContent = data['main']['temp'] + ' °C';
+    var feelsLikeElement = document.createElement('p');
+    feelsLikeElement.className = 'feels-like card-text fst-italic';
+    feelsLikeElement.textContent = 'Feels Like: ' + data['main']['feels_like'] + ' °C';
+
+    var tempElements = document.createElement('div');
+    tempElements.className = 'd-flex justify-content-around';
+    tempElements.appendChild(temperatureElement);
+    tempElements.appendChild(feelsLikeElement);
 
     var descElement = document.createElement('p');
     descElement.className = 'description card-text text-capitalize';
-    descElement.textContent = desc;
+    descElement.textContent = data['weather'][0]['description'];
 
-    cardBody.appendChild(tempElement);
+    cardBody.appendChild(tempElements);
     cardBody.appendChild(descElement);
     card.appendChild(cardHeader);
     card.appendChild(cardBody);
     col.appendChild(card);
 
-    resultsContainer.appendChild(col);
+    citiesSearchedContainer.appendChild(col);
 }
 
 function fetchWeather() {
@@ -57,11 +82,7 @@ function fetchWeather() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e9fdbec485cb1bb68cbd3557feb65d88&units=metric')
         .then(response => response.json())
         .then(data => {
-            var nameValue = data['name'];
-            var tempValue = data['main']['temp'];
-            var descValue = data['weather'][0]['description'];
-
-            createCityCard(nameValue, tempValue, descValue);
+            createCityCard(data);
         })
         .catch(e => alert("Please enter a valid city name."))
         .finally(() => {
